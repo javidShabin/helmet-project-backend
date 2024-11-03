b2c40482f9730890c1856d98e8cae45c59fb945f
javid shabin <javid.main.act@gmail.com>
2024-11-04 00:58:51 +0530

    Create the need controller bodyis only

---
+++ controllers/adminController.js
@@ -0,0 +1,39 @@
+// Admin registeration
+const adminregister = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Login admin
+const adminLogin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Logout admin
+const logoutAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Forget password
+const forgetAdminPassword = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Update admin
+const updateAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Check admin
+const checkAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  adminregister,
+  adminLogin,
+  logoutAdmin,
+  forgetAdminPassword,
+  updateAdmin,
+  checkAdmin,
+};
